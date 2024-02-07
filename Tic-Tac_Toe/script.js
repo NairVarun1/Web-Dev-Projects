dd1fc66024904f23763cf3b375f36a3e234b2aab
VARUN NAIR <137203305+NairVarun1@users.noreply.github.com>
2024-02-08 00:03:10 +0530

    Update script.js
    Proper positioning of the winning line

--- Tic-Tac_Toe/script.js
+++ Tic-Tac_Toe/script.js
@@ -16,7 +16,7 @@ const checkWin = () => {
     let boxtext = document.getElementsByClassName('boxtext');
     let wins = [
         [0, 1, 2, 5, 5, 0],
-        [3, 4, 5, 5, 5, 15],
+        [3, 4, 5, 5, 15, 0],
         [6, 7, 8, 5, 25, 0],
         [0, 3, 6, -5, 15, 90],
         [1, 4, 7, 5, 15, 90],
@@ -33,12 +33,13 @@ const checkWin = () => {
         ) {
             document.querySelector('.info').innerText = boxtext[e[0]].innerText + " Won";
             isgameover = true;
-            document.querySelector('.imgbox img').style.width = "400px";
-            document.querySelector('.line').style.width = "20vw";
-            document.querySelector('.line').style.transform =`translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`;
+            document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width = "200px";
+            document.querySelector(".line").style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`;
+            document.querySelector(".line").style.width = "20vw";
         }
     });
 };
+
 
 const checkDraw = () => {
     let boxtext = document.getElementsByClassName('boxtext');
